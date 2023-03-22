57b6b5db787b53fb834b4afdda2aac3693fd20c3
DarshDesai05 <darshdesai05@csu.fullerton.edu>
2023-03-22 14:31:01 -0700

    Second

--- test.js
+++ test.js
@@ -1,2 +1,16 @@
 const name = "Darshit"
 console.log(name);
+
+//Arrow Functions:-
+const greet = () => 'Hello World';
+console.log(greet());
+
+const bill = (products,tax)=> {
+    let total = 0;
+    for(let i = 0; i<products.length;i++){
+        total+= products[i] + products[i] * tax;
+    }
+    return total;
+};
+
+console.log(bill([10,15,20],0.2));
